63f7e2aed1b750939fa770ae62778129fefe5195
Maria Fantini <ma.fantini@duocuc.cl>
2025-10-14 20:39:27 -0300

    se cambió el logo de la app a una más elegante

--- app/src/main/java/com/example/gestion_bovina_grupo2/ui/theme/screens/LoginApp.kt
+++ app/src/main/java/com/example/gestion_bovina_grupo2/ui/theme/screens/LoginApp.kt
@@ -1,2 +1,24 @@
 package com.example.gestion_bovina_grupo2.ui.theme.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.gestion_bovina_grupo2.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.material3.MaterialTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginIngreso(){
+
+}
