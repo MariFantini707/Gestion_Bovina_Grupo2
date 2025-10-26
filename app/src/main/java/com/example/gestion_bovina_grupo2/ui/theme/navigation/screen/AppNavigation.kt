0846b31cba2107cf4ea5199cd3565b0ba16b2f4c
MariFantini707 <ma.fantini@duocuc.cl>
2025-10-26 20:53:40 -0300

    Se realizan arreglos en AppNavigation y BottomNavBar

--- app/src/main/java/com/example/gestion_bovina_grupo2/ui/theme/navigation/screen/AppNavigation.kt
+++ app/src/main/java/com/example/gestion_bovina_grupo2/ui/theme/navigation/screen/AppNavigation.kt
@@ -6,7 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.gestion_bovina_grupo2.ui.theme.screens.ResumenScreen
+
 import com.example.gestion_bovina_grupo2.ViewModel.UsuarioViewModel
 import com.example.gestion_bovina_grupo2.ui.theme.screens.LoginScreen
 
@@ -24,9 +24,7 @@ fun AppNavigation(){
         composable("registro"){
             LoginScreen(navController, usuarioViewModel)
         }
-        composable("resumen"){
-            ResumenScreen(usuarioViewModel)
-        }
+
     }
 }
 
